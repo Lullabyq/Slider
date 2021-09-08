b0524ea47181a08ca89f60de19f979cc94cd2d60
Lullabyq <abrashin012@gmail.com>
2021-09-08 19:08:43 +0300

    Adapted to mobile devices

--- Readme.md
+++ Readme.md
@@ -1,11 +1,13 @@
-# Awesome slider
+# Awesome responsive slider
 
 
-|№  | **Feature**      | **Description**|
-|:-:|------------------|----------------|
-|1  | Simple adding    |You can easily add a new slide by fulfilling just `4 steps`
-|2  | ProgressBar      |Client's able to see *amount of slides* and *position* on track of current slide
-|3  | Lurking Buttons  |When shown first or last slide corresponding button becomes hidden
+|№  | **Feature**        | **Description**|
+|:-:|--------------------|----------------|
+|1  | `Responsive design`| You're able to use this slider on both mobile device and desctop pc
+|2  | Autoscroll         | If user don't hover over the slider, it will scroll images automatically
+|3  | Simple adding      | You can easily add a new slide by fulfilling just `3 steps`
+|4  | ProgressBar        | Client's able to see *amount of slides* and *position* on track of current slide
+|5  | Lurking Buttons    | When shown first or last slide corresponding button becomes hidden
 
 <br>
 <br>
@@ -13,6 +15,7 @@
 ## Functionality of adding images:
 
 1) Add a new html tag
-2) Change attribute of generate-back() `mixin` in track.less
-3) Increase `count` variable in script.js
-4) Load a picture in `img` folder
+2) Increase `numberOfImages` variable in:
+    - script.js
+    - var.less
+3) Load a picture in `img` folder
--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+//0. To add img toggle this variable:
+let numberOfImages = 6
+// if you want change speed of slider:
+let timerDelay = 4000 // (in ms)
+
+
 // 1. Places buttons of slider
 let btnLeft = document.querySelector('.btn--left')
 let btnRight = document.querySelector('.btn--right')
@@ -17,6 +23,7 @@ function toggleBtnVisibility() {
     btnRight.classList.add('btn--disabled')
   } else if (position == 0) {
     btnLeft.classList.add('btn--disabled')
+    btnRight.classList.remove('btn--disabled')
   } else {
     btnRight.classList.remove('btn--disabled')
     btnLeft.classList.remove('btn--disabled')
@@ -34,13 +41,14 @@ function createInterval() {
     } else {
       btnRight.click()
     }
-  }, 4000)
+  }, timerDelay)
 }
 
 function createAutoScroll() {
   new Promise(res => {
     id = createInterval()
     res(id)
+    console.log(id);
   }).then(id => {
     box.addEventListener('mouseenter', clear)
   }).then(() => {
@@ -51,20 +59,44 @@ function createAutoScroll() {
 // EventListener callback func in order to remove listener
 function clear() {
   clearInterval(id)
+  flag = "on"
+  console.log("clear:", id);
 }
 
 // EventListener callback func in order to remove listener
 function set() {
   createAutoScroll()
+  flag = false
   box.removeEventListener('mouseenter', clear)
   box.removeEventListener('mouseleave', set)
 }
 
+function moveTrack (direction, initPosit) {
+  switch (direction) {
+    case 'left':
+      position -= imgWidth
+      position = Math.max(position, -trackLength)
+      break
+
+    case 'right':
+      position += imgWidth
+      position = Math.min(position, 0)
+      break
+
+    default: position = initPosit
+  }
+
+  track.style.left = position + 'px'
+  toggleBtnVisibility()
+}
+
 let track = document.querySelector('.track')
+let img = document.querySelector('.slider__img')
+let imgWidth = img.offsetWidth
 let position = 0
-let count = 6
-let trackLength = 1000 * (count - 1)
-let id
+let trackLength = imgWidth * (numberOfImages - 1)
+let id   // timer id
+let flag // indicator for resize listener
 
 createAutoScroll()
 toggleBtnVisibility()
@@ -74,15 +106,9 @@ box.addEventListener('click', function(event) {
   if (!target) return
 
   if (target.classList.contains("btn--right")) {
-    position -= 1000
-    position = Math.max(position, -trackLength)
-    track.style.left = position + 'px'
-    toggleBtnVisibility()
+      moveTrack('left')
     } else if (target.classList.contains("btn--left")) {
-    position += 1000
-    position = Math.min(position, 0)
-    track.style.left = position + 'px'
-    toggleBtnVisibility()
+      moveTrack('right')
   }
 })
 
@@ -96,15 +122,25 @@ for (let i = 0; i < images.length; i++) {
 
   images[i].dataset.progress = `${i + 1}/${images.length}`
   span.innerHTML = `${i + 1} / ${images.length}`
-
-  let top = images[i].offsetHeight - span.offsetHeight - 20
-  let left = images[i].offsetWidth - span.offsetWidth - 20
-
-  span.style.top = top + 'px'
-  span.style.left = left + 'px'
 }
 
 
+// 4 Adaptive features (308px is min divice maintained width)
 
+//This listener needs for better developer experiense with window resizing (user don't usually resize browser, esp. on mobile)
+window.addEventListener('resize', () => {
+  clearInterval(id)
+  console.log("clear_a", id);
+  let count = Math.abs(position / imgWidth)
+  let img = document.querySelector('.slider__img')//Dynamic link
 
+  imgWidth = img.offsetWidth // Current width of img
+  position = -imgWidth * count
+  trackLength = imgWidth * (numberOfImages - 1) // Update total length
 
+  // Resize on current slide feels weird:
+  // moveTrack('length', position)
+
+  moveTrack('custom', 0)
+  if (!flag) createAutoScroll() // in case you resize browser with mouseover the slider (ye, you can do it in devTools)
+})
