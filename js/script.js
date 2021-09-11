6c7764e364247577782d0e5d55392353f2465ec2
Lullabyq <abrashin012@gmail.com>
2021-09-11 21:53:48 +0300

    Enabled touch control over slider
    After use of scroll-snap functionality stuck with problems:
    - rapid swipes with buttons
    - lack of swipes on desktop (only by btns)
    - ugly scrollbar (solved)
    Also logic of changing slides was updated

--- js/script.js
+++ js/script.js
@@ -8,7 +8,6 @@ let timerDelay = 4000 // (in ms)
 let btnLeft = document.querySelector('.btn--left')
 let btnRight = document.querySelector('.btn--right')
 let box = document.querySelector('.container__btn')
-
 let translate = btnLeft.offsetWidth / 2
 
 btnLeft.style.left = -translate + 'px'
@@ -16,12 +15,14 @@ btnLeft.style.top = box.offsetHeight / 2 -translate + 'px'
 btnRight.style.top = box.offsetHeight / 2 -translate + 'px'
 btnRight.style.right = -translate + 'px'
 
+box.clientHeight = box.offsetHeight
+
 
 // 2. (Main part) Makes slider work
 function toggleBtnVisibility() {
-  if (position == -trackLength) {
+  if (trackLength - track.scrollLeft < imgWidth / 2) {
     btnRight.classList.add('btn--disabled')
-  } else if (position == 0) {
+  } else if (track.scrollLeft < imgWidth / 2 ) {
     btnLeft.classList.add('btn--disabled')
     btnRight.classList.remove('btn--disabled')
   } else {
@@ -34,8 +35,8 @@ function toggleBtnVisibility() {
 // When mouseout event happens slider starts autoscroll
 function createInterval() {
   return setInterval(() => {
-    if (position == -trackLength) {
-      position = 0
+    if (track.scrollLeft == trackLength) {
+      track.scrollLeft = 0
       btnRight.classList.remove('btn--disabled')
       btnLeft.click()
     } else {
@@ -70,34 +71,37 @@ function set() {
 }
 
 function moveTrack (direction, initPosit) {
-  switch (direction) {
-    case 'left':
-      position -= imgWidth
-      position = Math.max(position, -trackLength)
-      break
+  let position = track.scrollLeft
 
+  switch (direction) {
     case 'right':
       position += imgWidth
-      position = Math.min(position, 0)
+      position = Math.min(position, trackLength)
+      break
+
+    case 'left':
+      position -= imgWidth
+      position = Math.max(position, 0)
       break
 
     default: position = initPosit
   }
-
-  track.style.left = position + 'px'
+  track.scrollLeft = position
   toggleBtnVisibility()
 }
 
 let track = document.querySelector('.track')
 let img = document.querySelector('.slider__img')
 let imgWidth = img.offsetWidth
-let position = 0
 let trackLength = imgWidth * (numberOfImages - 1)
 let id   // timer id
-let flag // indicator for resize listener
+let flag // indicator for resize listentrack.scrollLeft
 
 createAutoScroll()
 toggleBtnVisibility()
+
+
+//============Event listeners Block======================
 
 box.addEventListener('click', function(event) {
 
@@ -115,20 +119,38 @@ box.addEventListener('click', function(event) {
       text.data = 'Show description'
       interface.classList.add('slider__text--hidden')
       text.parentNode.classList.add('checkboxContainer__top')
-
     }
   }
-
   // Slide images
   let target = event.target.closest('button')
   if (!target) return
 
   if (target.classList.contains("btn--right")) {
-      moveTrack('left')
-    } else if (target.classList.contains("btn--left")) {
       moveTrack('right')
+    } else if (target.classList.contains("btn--left")) {
+      moveTrack('left')
   }
 })
+
+box.addEventListener('touchmove', () => {
+  setTimeout(toggleBtnVisibility, 200)
+})
+
+// In order to use touch swipes on mobile
+function disableBtn() {
+  if (document.documentElement.clientWidth < 575) {
+    btnLeft.style.display = 'none'
+    btnRight.style.display = 'none'
+  } else {
+    btnLeft.style.display = ''
+    btnRight.style.display = ''
+  }
+}
+
+window.addEventListener('resize', disableBtn)
+window.addEventListener('load', disableBtn)
+
+// =============================================================
 
 
 // 3. Places progressBar for slider
@@ -142,24 +164,17 @@ for (let i = 0; i < images.length; i++) {
   span.innerHTML = `${i + 1} / ${images.length}`
 }
 
-// 4
-let checkbox = document.querySelector('.checkboxContainer')
-let checkboxText = checkbox.firstChild
 
-
-
-
-
-// 5 Adaptive features (308px is min divice maintained width)
+// 4 Adaptive features (308px is min divice maintained width)
 
 //This listener needs for better developer experiense with window resizing (user don't usually resize browser, esp. on mobile)
 window.addEventListener('resize', () => {
   clearInterval(id)
-  let count = Math.abs(position / imgWidth)
+  let count = Math.abs(track.scrollLeft / imgWidth)
   let img = document.querySelector('.slider__img')//Dynamic link
 
   imgWidth = img.offsetWidth // Current width of img
-  position = -imgWidth * count
+  track.scrollLeft = -imgWidth * count
   trackLength = imgWidth * (numberOfImages - 1) // Update total length
 
   // Resize on current slide feels weird:
